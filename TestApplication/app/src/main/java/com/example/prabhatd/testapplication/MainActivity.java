84748be05df01463d3382119dedd78de20759ee6
Prabhat Dwivedi <prabhatd@pune-com-dt-014.payso.in>
2018-12-04 15:37:29 +0530

    Androidd app

---
+++ TestApplication/app/src/main/java/com/example/prabhatd/testapplication/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.prabhatd.testapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+    }
+}
